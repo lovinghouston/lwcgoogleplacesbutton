c778f3fefaea03dc6396fada94162a3a329a8938
Sunghyun Song <sunghyun.song@sunghyunso-ltm1.internal.salesforce.com>
2020-03-04 22:07:14 -0800

    Refactored some of GooglePlaces and added LWC component to call on the Apex method via front-end

--- force-app/main/default/lwc/apiCallButton/apiCallButton.js
+++ force-app/main/default/lwc/apiCallButton/apiCallButton.js
@@ -1,3 +1,57 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import searchPlace from '@salesforce/apex/GooglePlaces.searchPlace';
+import BillingPostalCode from '@salesforce/schema/Account.BillingPostalCode';
+import BillingLongitude from '@salesforce/schema/Account.BillingLongitude';
+import BillingLatitude from '@salesforce/schema/Account.BillingLatitude';
 
-export default class ApiCallButton extends LightningElement {}
+
+const FIELDS = [
+    'Account.BillingPostalCode',
+    'Account.BillingLatitude',
+    'Account.BillingLongitude',
+];
+
+const columns = [
+    {label: 'CSP Id', fieldName: 'Id'},
+    {label: 'School', fieldName: 'School_Name__c'},
+    {label: 'Church', fieldName: 'Church_Name__c'},
+    {label: 'Status', fieldName: 'Status__c'},
+    {label: 'Name', fieldName: 'Name'}
+];
+
+export default class ApiCallButton extends LightningElement {
+
+    @track showCSPs = false;
+    @api recordId;
+    @track csps;
+    @track columns = columns;
+
+    @wire(getRecord, {recordId: '$recordId', fields: FIELDS})
+    account;
+
+    /*
+    postalCode = getFieldValue(this.account., BillingPostalCode);
+    billingLatitude = getFieldValue(this.account.fields, BillingLatitude);
+    billingLongitude = getFieldValue(this.account.fields, BillingLongitude);
+    */
+
+    handleFindChurchesClick() {
+        console.log(this.account.BillingPostalCode);
+        searchPlace({
+            account: this.recordId,
+            longitude: this.account.data.fields.BillingLongitude.value,
+            latitude: this.account.data.fields.BillingLatitude.value,
+            radius: 5,
+            type: 'church',
+            key: 'AIzaSyBJYW5TNtGJ10l9CxUoy0RHJSb6zlbilPk'
+        }).then(result => {
+            console.log(result);
+            this.csps = result;
+            this.showCSPs = true;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
